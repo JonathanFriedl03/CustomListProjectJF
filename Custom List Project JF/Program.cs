90ebde386b673b24edc0697930ce6ef6ed19e111
Jonathan Friedl <jfreetz2@gmail.com>
2020-02-24 10:22:04 -0600

    Intial repository for this project

---
+++ Custom List Project JF/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_List_Project_JF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
